a02ea18a9f55931679169e02ef3fe59d01bad157
Brice Dereims <bdereims@gmail.com>
2020-12-03 16:58:14 +0100

    update a lot...

---
+++ kubeapps/install-kubeapps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+kubectl create namespace kubeapps
+helm install kubeapps --namespace kubeapps bitnami/kubeapps
+
+kubectl create serviceaccount kubeapps-operator
+kubectl create clusterrolebinding kubeapps-operator --clusterrole=cluster-admin --serviceaccount=default:kubeapps-operator
