2b4133251bebfbbfc68194d228f1a3c01f5c765c
Brice Dereims <bdereims@gmail.com>
2021-06-04 13:32:21 +0200

    add tkgm proxy/noproxy

--- tkgm/1.3.x/3-initialize.sh
+++ tkgm/1.3.x/3-initialize.sh
@@ -3,16 +3,17 @@
 
 . ../../env
 
-rm -fr ~/.tanzu/tkg/bom
+rm -fr ~/.tanzu
 export TKG_BOM_CUSTOM_IMAGE_TAG="v1.3.1-patch1"
 echo "VSPHERE_INSECURE: true" > ~/.tanzu/tkg/cluster-config.yaml
 
 IP=$( ip a show dev ens192 | grep inet | sed -e "s/^.*inet //" -e "s/\/.*$//" )
 
 ### Add subnets for TKG-Portgroup and Frontend
-#export no_proxy=vcsa.${DOMAIN},tkgm.${DOMAIN},172.17.20.100,${IP}
-export no_proxy=172.17.20.0/24,10.30.0.0/24
+export no_proxy=192.168.0.0/16,172.16.0.0/12,10.0.0.0/8,*.${DOMAIN}
 echo "no_proxy for: ${no_proxy}"
 
+### If you prefer to setup trough ui
 #tanzu management-cluster create --ui --bind ${IP}:8080 --browser none -v 9
+
 tanzu management-cluster create --file cluster-config.yaml -v 9
